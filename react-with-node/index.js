161406c9ab3751248d1120d1da7ce6edcd81b1cd
Adel <adel.adel@vodafone.com>
2019-07-15 00:05:20 +0200

    Add code

--- react-with-node/index.js
+++ react-with-node/index.js
@@ -9,15 +9,22 @@ passport.use(new googleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, (accessToken) => {
+}, (accessToken, refreshToken, profile, done) => {
     console.log(accessToken)
+    console.log(refreshToken);
+    console.log(profile);
+    console.log(done)
 }));
 
 app.get('/auth/google', passport.authenticate('google', {
     scope: ['profile', 'email']
 }));
 
-
+app.get('/adool',(req,res)=>{
+    res.json(({
+        text:'hey'
+    }))
+})
 
 
 const port = process.env.port || 5000;
