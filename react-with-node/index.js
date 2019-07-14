f7f49215b23c223deb3f8fa6b997f6c87914cf2a
Adel Amr <os@OSs-MacBook-Pro.local>
2019-07-14 10:10:23 +0200

    Add notes

--- react-with-node/index.js
+++ react-with-node/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth20');
+const keys = require('./config/keys')
+
 
 const app = express();
-passport.use(new googleStrategy());
+passport.use(new googleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+    console.log(accessToken)
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+
 
 
 const port = process.env.port || 5000;
